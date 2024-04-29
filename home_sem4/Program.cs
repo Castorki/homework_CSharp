45b96068c0f16698f80dea6dbe963966e692d933
Darya Kauzova <amayasuzuki12@gmail.com>
2024-04-29 11:15:26 +0300

    Made homewor for seminar 4

---
+++ home_sem4/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+
+
+
+// Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(99, 999);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// int Counter(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0)
+//         {
+//             count = count + 1;
+//         }
+//     }
+//     return count;
+// }
+
+// System.Console.WriteLine("Input array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreateArray(size);
+// PrintArray(array);
+// System.Console.WriteLine(Counter(array));
+
+
+// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+// int[] CreateArray(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// int[] ReversArray(int[] array)
+// {
+//     int[] reversArr = new int[array.Length];
+//     for (int i = array.Length - 1, b = 0; i >= 0; b++, i--)
+//     {
+//         reversArr[b] = array[array.Length - 1 - b];
+//     }
+//     return reversArr;
+// }
+
+// System.Console.WriteLine("Input array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input minimal value of arr element: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input maximal value of arr element: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreateArray(size, min, max);
+// PrintArray(array);
+// PrintArray(ReversArray(array));
